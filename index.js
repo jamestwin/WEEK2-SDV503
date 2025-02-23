f1c67a2f2c7daa8c9da626256b164b2efd5a2947
cryptictwin <james-twin@live.nmit.ac.nz>
2025-02-24 11:23:05 +1300

    basic JS code, learning about const and let variables

---
+++ index.js
@@ -0,0 +1,10 @@
+// console.log('James Twin');
+// Learning JS in SDV503 class
+/* multi-line comment example
+Hello
+You see?
+*/
+let a = 12
+let b = 10
+const result = a + b
+console.log(`The result of a + b = ${result}`) 
